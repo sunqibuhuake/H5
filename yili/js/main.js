ece621a33ce9913e4ad6e8769d5412ed97bd3a87
没有bug的无聊世界 <12824031@qq.com>
2016-03-31 16:11:13 +0800

    delete height setting

--- yili/js/main.js
+++ yili/js/main.js
@@ -1,7 +1,6 @@
 var ps = new brickjs.PageSwitch({
     pages: '.page',
     onSwitchEnd: function() {
-        $('.page').height($('.page').width()*1136/640);
         $('.page>.wrapper').removeClass('current').eq(ps.curPage).addClass('current');
     }
 })
