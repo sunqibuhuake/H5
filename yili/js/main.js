b6f7e60924a6a29e4b10b4e33fccdd351d0eacf8
没有bug的无聊世界 <12824031@qq.com>
2016-04-01 15:32:45 +0800

    add page 5

--- yili/js/main.js
+++ yili/js/main.js
@@ -1,6 +1,21 @@
+var g3_cao = document.querySelector('#cao');
+g3_cao.addEventListener("webkitAnimationEnd", function () {
+    this.className = 'cao';
+}, false);
+
+var g3_circleArrow = document.querySelector('#circleArrow');
+g3_circleArrow.addEventListener("webkitAnimationEnd", function () {
+    this.className = 'circleArrow';
+}, false);
+
 var ps = new brickjs.PageSwitch({
     pages: '.page',
-    onSwitchEnd: function() {
+    onSwitchEnd: function () {
+
+        g3_cao.className = '';
+        g3_circleArrow.className = '';
+
         $('.page>.wrapper').removeClass('current').eq(ps.curPage).addClass('current');
     }
-})
+});
+
