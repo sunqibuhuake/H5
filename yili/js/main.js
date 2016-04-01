12cb0eb6d2e5d2fda6ee50be5258ca156de0f232
没有bug的无聊世界 <12824031@qq.com>
2016-04-01 19:09:56 +0800

    add loading effect

--- yili/js/main.js
+++ yili/js/main.js
@@ -1,26 +1,29 @@
-var open_ear = document.querySelector('#ear');
-open_ear.addEventListener("webkitAnimationEnd", function() {
-    this.className = 'ear';
-})
-var g3_cao = document.querySelector('#cao');
-g3_cao.addEventListener("webkitAnimationEnd", function () {
-    this.className = 'cao';
-}, false);
 
-var g3_circleArrow = document.querySelector('#circleArrow');
-g3_circleArrow.addEventListener("webkitAnimationEnd", function () {
-    this.className = 'circleArrow';
-}, false);
 
-var ps = new brickjs.PageSwitch({
-    pages: '.page',
-    onSwitchEnd: function () {
+function init() {
+    var open_ear = document.querySelector('#ear');
+    open_ear.addEventListener("webkitAnimationEnd", function() {
+        this.className = 'ear';
+    })
+    var g3_cao = document.querySelector('#cao');
+    g3_cao.addEventListener("webkitAnimationEnd", function () {
+        this.className = 'cao';
+    }, false);
 
-        g3_cao.className = '';
-        g3_circleArrow.className = '';
-        open_ear.className = '';
-
-        $('.page>.wrapper').removeClass('current').eq(ps.curPage).addClass('current');
-    }
-});
-
+    var g3_circleArrow = document.querySelector('#circleArrow');
+    g3_circleArrow.addEventListener("webkitAnimationEnd", function () {
+        this.className = 'circleArrow';
+    }, false);
+    var ps = new brickjs.PageSwitch({
+        pages: '.page',
+        onSwitchEnd: function () {
+            g3_cao.className = '';
+            g3_circleArrow.className = '';
+            open_ear.className = '';
+            $('.page>.wrapper').removeClass('current').eq(ps.curPage).addClass('current');
+        }
+    });
+    $('.Open .wrapper').addClass('current');
+    $('.loading').addClass('goaway');
+    $('.loading').html('');
+}
